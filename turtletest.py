ebb34614d9996a005c888fc5be8031d3087a8bcb
Tom Chen <tom@itri.org.tw>
2020-01-04 22:40:29 +0800

    Add turtle and var test

---
+++ turtletest.py
@@ -0,0 +1,28 @@
+
+
+"""
+
+要學多久
+
+Version: 0.0.1
+Author: TOMJR
+"""
+
+
+import turtle
+
+# turtle.Vec2D
+turtle.speed(1)  # slowest speed is 1
+
+turtle.pensize(8)
+turtle.pencolor('red')
+
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(200)
+
+turtle.mainloop()
---
+++ vartest.py
@@ -0,0 +1,19 @@
+
+
+"""
+練習variant
+
+Version: 0.1
+Author: TOMJR
+
+"""
+
+aVar = int(input("a = "))
+bVar = int(input("b = "))
+
+print("%d / %d = %f" % (aVar, bVar, aVar / bVar))
+print("%d ** %d = %d" % (aVar, bVar, aVar ** bVar))
+print("%d %% %d = %d" % (aVar, bVar, aVar % bVar))
+print("%d // %d = %d" % (aVar, bVar, aVar // bVar))
+
+
